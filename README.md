8c5d97ba3bcaf80f7fa23aae4a3c95d874acaa14
Mihaela <mihaelavalac@yahoo.com>
2020-10-31 22:18:02 -0400

    readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# password-generator
+# Project Description:
+This project required to develop an js program in order to generate random a password. The html and css file were already provided and my goal was to make the page to interact and respond to the user imputs. 
+
+
+# My contribution:
+I used variables, contitional statemens , and functions in order to make the page to work. 
+
+# Level of Dificulty:
+While working on this project i realize how powerful are the functions. Althought I didn't use functions very much in this project, I plan to study in deph this concept and to implement it in my further projects. 
+
+# Deployment link:
+https://mihaelavalac.github.io/password-generator/
--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var passwordText = document.querySelector("#password");
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var nrs = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var spchar = ['@', '%', '+', '', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.']
+var spchar = ["@", "%", "+", " ", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}" , "{" , "]" , "[", "~", "-", "_", "."]
 
 
 generateBtn.addEventListener("click", generatePassword);
